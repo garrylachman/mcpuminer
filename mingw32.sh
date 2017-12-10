608d31ae04a4bcb0ce89a5360afee4baaf14adbd
Ballagyr <ballagyr@openvms.eu.org>
2017-12-10 21:22:28 -0200

    Cross compilation for Win32. v2

--- mingw32.sh
+++ mingw32.sh
@@ -1,4 +1,13 @@
 ./autogen.sh
 
+extracflags="-O3 -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fvariable-expansion-in-unroller -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
+CFLAGS="-DCURL_STATICLIB -DOPENSSL_NO_ASM -DUSE_ASM -static-libgcc $extracflags"
+
 mingw32-configure --build=mingw32 --with-crypto --with-curl CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS"
 mingw32-make
+
+strip -p --strip-debug --strip-unneeded cpuminer.exe
+
+if [ -e sign.sh ] ; then
+. sign.sh
+fi
